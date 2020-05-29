e96450756f7ed4da5149bce6574489518fc0c0ac
Jan Puto <tofukles@gmail.com>
2020-05-29 14:25:01 +0200

    Save system 0.1

--- DemoForm/StorageUtilityFunctions.cs
+++ DemoForm/StorageUtilityFunctions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BankClassLibrary;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,64 @@ namespace DemoForm
     // Metody i właściwości używane do zapisu w plikach
     public static class StorageUtilityFunctions
     {
+        const string TransactionStorageDir = @"..\datebase\transactions";
+        const string CustomerStorageDir = @"..\database\customer";
+
+        static bool CheckMainDirectorys()
+        {
+            if (Directory.Exists(TransactionStorageDir))
+            {
+                if (Directory.Exists(CustomerStorageDir))
+                {
+                    return true;
+                }
+                else
+                {
+                    Directory.CreateDirectory(CustomerStorageDir);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(TransactionStorageDir);
+            }
+            return true;                        
+        }
+
+        public static Account GetLastAccount()
+        {
+            return null;
+        }
+
+        public static bool SaveAccount(Account aAccount)
+        {
+            // File name        AccountID.dat
+            // Pierwsza lini    AccountID_CustomerName
+            // Kolejne linie    TransactionType_TransactionAmount_TransactionDate_Location
+
+           
+            using(StreamWriter sw =
+                File.CreateText(TransactionStorageDir + "\\" + aAccount.AccountNumber + ".dat"))
+            {
+                sw.WriteLine($"{aAccount.AccountNumber}_{aAccount.CustomerName}");
+
+                foreach(Transaction tr in aAccount.ListOfTransactions)
+                {
+                    string transactionLine = $"{tr.TransactionTypeString}_{tr.MoneyAmount}_{tr.TransactionDateString}_{tr.Location}";
+
+                    sw.WriteLine(transactionLine);
+                }
+            }
+
+            // File name        CustomerName.dat
+            // Pierwsza linia   CustomerName_BirthDate_Phone
+            // Kolejne linie    AddressLine1\nAddressLine2\nAddressLine3 ...
+
+            
+
+
+
+            return false;
+        }
+
     }
 }
